85a4eac69efa9103818c8ab88325cfeead99d7af
HauTran <98hautran@gmail.com>
2022-08-13 10:47:23 +0700

    Nhap Ho Ten

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<string.h>
+using namespace std;
+
+int main()
+{
+
+    string hoten;
+    cout<<"Moi nhap ten:";
+    getline(cin,hoten);
+    cout<<"Xin Chao thang LZ "<< hoten <<endl;
+    return 0;
+}
