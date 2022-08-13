098264e7b72f1ed2cb83f40c24797519b3f4f898
HauTran <98hautran@gmail.com>
2022-08-13 10:59:51 +0700

    Ask Age

--- main.cpp
+++ main.cpp
@@ -4,10 +4,16 @@ using namespace std;
 
 int main()
 {
-
+    int tuoi;
     string hoten;
     cout<<"Moi nhap ten:";
     getline(cin,hoten);
     cout<<"Xin Chao "<< hoten <<" dep trai" <<endl;
+    cout<<"Xin hoi "<< hoten <<" dep trai bao nhieu tuoi: " <<endl; 
+    cin>>tuoi;
+    cout<< "Thong tin ca nhan"<<endl;
+    cout<<"Ho Ten: "<<hoten<<endl;
+    cout<<"Tuoi: "<<tuoi<<endl;
+
     return 0;
 }
